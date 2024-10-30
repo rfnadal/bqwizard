ee26448d103e662adf08f6c82cd36065aaae07b2
Rolando Franqui <rolandofranqui@gmail.com>
2024-10-29 22:04:15 -0400

    Made the dataset info command list the tables within the dataset

--- bqwizard/main.py
+++ bqwizard/main.py
@@ -16,18 +16,20 @@ app.add_typer(table, name="table")
 @dataset.command(name="info")
 def get_dataset_info(dataset_id: str):
   dataset = client.get_dataset(dataset_id)
-  full_dataset_id = "{}.{}".format(dataset.project, dataset.dataset_id)
+  full_dataset_id = f"{dataset.project}.{dataset.dataset_id}"
   print(f"Here is the info for the {dataset_id} dataset")
-  print("Description: {}".format(dataset.description))
+  print(f"Description: {dataset.description}")
+  print(f"Tables within the dataset: {[table.table_id for table in client.list_tables(dataset)]}")
+
 
 @table.command(name="info")
 def get_table_info(table_id: str):
   table = client.get_table(table_id)
-  print("Got table '{}.{}.{}'.".format(table.project, table.dataset_id, table.table_id))
-  print("Table schema: {}".format(table.schema))
-  print("Table description: {}".format(table.description))
-  print("Table has {} rows".format(table.num_rows))
-  print("Table was last updated {} rows".format(table.modified))
+  print(f"Got table '{table.project}.{table.dataset_id}.{table.table_id}'.")
+  print(f"Table schema: {table.schema}")
+  print(f"Table description: {table.description}")
+  print(f"Table has {table.num_rows} rows")
+  print(f"Table was last updated {table.modified} rows")
 
 @table.command(name="freshness")
 def get_table_freshness(table_id: str):
@@ -35,7 +37,6 @@ def get_table_freshness(table_id: str):
   current_time = datetime.utcnow()
   last_modified_dt = datetime.fromtimestamp(table.modified.timestamp())
   print(f"The {table_id} is currently this old: {current_time - last_modified_dt}")
-
 
 
 if __name__ == "__main__":
