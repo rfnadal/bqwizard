b584de7ef3f69d9edd44fe8c29d35b6bb0b57485
Rolando Franqui <rolandofranqui@gmail.com>
2025-04-04 09:55:04 -0400

    feat: Added a docstring to the table command group.

--- src/bqwizard/commands/table.py
+++ src/bqwizard/commands/table.py
@@ -6,6 +6,9 @@ from tabulate import tabulate
 @click.group()
 @click.pass_context
 def table(ctx: Context):
+    """
+    A group of commands for managing tables.
+    """
     pass
 
 
@@ -28,20 +31,32 @@ def describe(ctx: Context, table: str):
         raise click.Abort()
     table_id = f"{project}.{table}"
     table_ref = client.get_table(table_id)
-    table_data = [[
-        table_ref.created.strftime("%B %d, %Y at %I:%M %p UTC"),
-        table_ref.modified.strftime("%B %d, %Y at %I:%M %p UTC"),
-        f"{table_ref.num_rows:,}",
-        table_ref.table_type,
-        f"{table_ref.num_bytes / 1000000:.2f} MB",
-        table_ref.partitioning_type or "None",
-        ", ".join(table_ref.clustering_fields) if table_ref.clustering_fields else "None",
-    ]]
+    table_data = [
+        [
+            table_ref.created.strftime("%B %d, %Y at %I:%M %p UTC"),
+            table_ref.modified.strftime("%B %d, %Y at %I:%M %p UTC"),
+            f"{table_ref.num_rows:,}",
+            table_ref.table_type,
+            f"{table_ref.num_bytes / 1000000:.2f} MB",
+            table_ref.partitioning_type or "None",
+            ", ".join(table_ref.clustering_fields)
+            if table_ref.clustering_fields
+            else "None",
+        ]
+    ]
     click.echo(f"\nDisplaying the data for: {table_id} \n")
     click.echo(
         tabulate(
             table_data,
-            headers=["Created", "Modified", "Rows", "Type", "Size", "Partition", "Cluster"],
+            headers=[
+                "Created",
+                "Modified",
+                "Rows",
+                "Type",
+                "Size",
+                "Partition",
+                "Cluster",
+            ],
             tablefmt="simple",
         )
     )
